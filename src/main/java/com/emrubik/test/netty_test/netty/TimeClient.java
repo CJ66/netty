77b6f2a20bfbbf9f8b50305a53a46380b3b65f35
changj <changj@emrubik.com>
2016-12-08 15:58:59 +0800

    提交

--- src/main/java/com/emrubik/test/netty_test/netty/TimeClient.java
+++ src/main/java/com/emrubik/test/netty_test/netty/TimeClient.java
@@ -14,6 +14,8 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
 
 /**
  * TODO 这里请补充该类型的简述说明
@@ -34,6 +36,8 @@ public class TimeClient {
                 @Override
                 protected void initChannel(SocketChannel ch) throws Exception {
                     // TODO Auto-generated method stub
+                    ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                    ch.pipeline().addLast(new StringDecoder());
                     ch.pipeline().addLast(new TimeClientHandler());
                 }
             });
