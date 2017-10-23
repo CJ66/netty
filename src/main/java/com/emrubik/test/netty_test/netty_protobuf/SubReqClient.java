272002dfc129890c48b501b42804993385294b13
changj <changj@emrubik.com>
2017-10-23 17:32:18 +0800

    netty protobuf 修改

---
+++ src/main/java/com/emrubik/test/netty_test/netty_protobuf/SubReqClient.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * @(#)SubReqClient.java 2017年2月6日
+ *
+ * Copyright 2017 emrubik Group Ltd. All rights reserved.
+ * EMRubik PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *******************************************************************************/
+package com.emrubik.test.netty_test.netty_protobuf;
+
+import com.emrubik.test.netty_test.netty.TimeClient;
+import com.emrubik.test.netty_test.netty.TimeClientHandler;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.protobuf.ProtobufDecoder;
+import io.netty.handler.codec.protobuf.ProtobufEncoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * TODO 这里请补充该类型的简述说明
+ * @author <a href="mailto:changj@emrubik.com">chang jiang</a>
+ * @version $Revision 1.0 $ 2017年2月6日 下午12:01:35
+ */
+public class SubReqClient {
+    public void connect(int port,String host) throws Exception{
+        //配置客户端NIO线程组
+        EventLoopGroup group = new NioEventLoopGroup();
+        try{
+            Bootstrap b = new Bootstrap();
+            b.group(group).channel(NioSocketChannel.class)
+            .option(ChannelOption.TCP_NODELAY, true)
+            .handler(new ChannelInitializer<SocketChannel>() {
+
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    // TODO Auto-generated method stub
+                    ch.pipeline().addLast(new ProtobufVarint32FrameDecoder());
+                    ch.pipeline().addLast(new ProtobufDecoder(SubscribeRespProto.SubscribeResp.getDefaultInstance()));
+                    ch.pipeline().addLast(new ProtobufVarint32LengthFieldPrepender());
+                    ch.pipeline().addLast(new ProtobufEncoder());
+                    ch.pipeline().addLast(new SubReqClientHandler());
+                }
+            });
+            //发起异步连接操作
+            ChannelFuture f = b.connect(host, port).sync();
+            //等待客户端链路关闭
+            f.channel().closeFuture().sync();
+        }finally{
+            //优雅退出，释放NIO线程组
+            group.shutdownGracefully();
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if(args != null && args.length>0){
+            try{
+                port = Integer.valueOf(args[0]);
+            }catch(NumberFormatException e){
+                
+            }
+        }
+        new SubReqClient().connect(port, "127.0.0.1");
+    }
+}
---
+++ src/main/java/com/emrubik/test/netty_test/netty_protobuf/SubscribeReq.java
@@ -0,0 +1,81 @@
+/*******************************************************************************
+ * @(#)SubscribeReq.java 2017年2月6日
+ *
+ * Copyright 2017 emrubik Group Ltd. All rights reserved.
+ * EMRubik PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *******************************************************************************/
+package com.emrubik.test.netty_test.netty_protobuf;
+
+import java.io.Serializable;
+
+/**
+ * TODO 这里请补充该类型的简述说明
+ * @author <a href="mailto:changj@emrubik.com">chang jiang</a>
+ * @version $Revision 1.0 $ 2017年2月6日 上午11:33:00
+ */
+public class SubscribeReq implements Serializable{
+
+    /**
+     * serialVersionUID : TODO 这里请补充该字段的简述说明
+     */
+    private static final long serialVersionUID = 1L;
+    
+    private int subReqID;
+    
+    private String userName;
+    
+    private String productName;
+    
+    private String phoneNumber;
+    
+    private String address;
+
+    public int getSubReqID() {
+        return subReqID;
+    }
+
+    public void setSubReqID(int subReqID) {
+        this.subReqID = subReqID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "SubscribeReq [subReqID=" + subReqID + ", userName=" + userName + ", productName="
+                + productName + ", phoneNumber=" + phoneNumber + ", address=" + address + "]";
+    }
+    
+    
+
+}
